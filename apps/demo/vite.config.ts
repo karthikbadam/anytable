26533725f4fb549901cfa6d8aa24b7d53c57b192
Karthik Badam <sriram.karthik6@gmail.com>
2026-02-08 10:38:38 -0800

    update: name

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/anytable/' : '/',
+  base: process.env.GITHUB_PAGES ? '/any_table/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@duckdb/duckdb-wasm'],
